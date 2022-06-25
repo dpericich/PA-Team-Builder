a564dc9f7ee78f2a89226c58bf673ed9673f6b71
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-06-25 00:11:54 -0500

    updated getPokemonData axios call to run middleware before external api call

--- src/helpers/pokeapi.ts
+++ src/helpers/pokeapi.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
+import { hisuianPokemon, variantTypePokemon } from './pokemon';
 
 const BASE_POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
 
-const getPokemonData = async (pokemonName: string) => {
-    const pokemonResponse = await axios.get(`${BASE_POKE_API_URL}${pokemonName.toLowerCase()}`);
+const getPokemonData = async (pokemonName: number) => {
+    if (hisuianPokemon[pokemonName]) {
+        return hisuianPokemon[pokemonName]
+    }
+    
+    const pokemonResponse = await axios.get(`${BASE_POKE_API_URL}${pokemonName}`);
     const pokemonData = pokemonResponse.data;
     return pokemonData;
 }
+
+export const hisuianPokemonNumbers: number[] = Object.keys(hisuianPokemon).map(i => parseInt(i))
 
 export default getPokemonData;
